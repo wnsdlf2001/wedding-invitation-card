03a5ff6b092d35cb186a70e4cef19b643de4cd8a
wnsdlf2001 <wnsdlf2001@gmail.com>
2025-02-01 12:36:53 +0900

    github page setting

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: '/wedding-invitation-card',
+  assetPrefix: '/wedding-invitation-card/'
+
 };
 
 export default nextConfig;
