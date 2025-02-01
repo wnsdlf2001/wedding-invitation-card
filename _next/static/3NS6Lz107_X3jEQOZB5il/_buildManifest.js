d575cc99b0ec719bd0522b26ece8854f093dcb04
wnsdlf2001 <wnsdlf2001@gmail.com>
2025-02-01 18:56:43 +0900

    Updates

--- _next/static/3NS6Lz107_X3jEQOZB5il/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/575db74b3b6e9ef8.css","static/chunks/pages/index-da057ad930592ddf.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
