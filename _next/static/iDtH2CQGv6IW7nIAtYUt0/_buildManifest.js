8bb60f803dc96a27785788d0a72890cc933f44ff
Yoon Joon Il <wnsdlf2001@gmail.com>
2025-05-30 01:14:02 +0900

    Updates

---
+++ _next/static/iDtH2CQGv6IW7nIAtYUt0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-59fdf0e0b10a7f35.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
